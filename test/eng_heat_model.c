a458eb5defb5bc50361967f6f2f5a754fbb41cf9
Matthias Röthlin <mroethli@ethz.ch>
2017-06-09 17:22:01 +0200

    licensed .c files aswell...

--- test/eng_heat_model.c
+++ test/eng_heat_model.c
@@ -1,3 +1,20 @@
+//Copyright ETH Zurich, IWF
+
+//This file is part of iwf_thermal.
+
+//iwf_thermal is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//iwf_thermal is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with iwf_thermal.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "eng_heat_model.h"
 
 #define MAX_STEPS 50000
