850a9d100e92129b438348df7d8477580321d7f5
Matthias Röthlin <mroethli@ethz.ch>
2017-06-12 10:08:08 +0200

    added debug make files

---
+++ Debug/structures/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../structures/particle.c \
+../structures/singleton_geometry.c \
+../structures/singleton_physics.c 
+
+OBJS += \
+./structures/particle.o \
+./structures/singleton_geometry.o \
+./structures/singleton_physics.o 
+
+C_DEPS += \
+./structures/particle.d \
+./structures/singleton_geometry.d \
+./structures/singleton_physics.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+structures/%.o: ../structures/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -std=gnu99 -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
